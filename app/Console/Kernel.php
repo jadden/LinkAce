3f1e484b9c2e759a284daa99a03e2a7b8e4bcd9d
Kovah <mail@kovah.de>
2025-01-30 13:27:23 +0100

    Cap number of jobs ran via the schedule at one run

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,7 +9,7 @@ class Kernel extends ConsoleKernel
 {
     protected function schedule(Schedule $schedule): void
     {
-        $schedule->command('queue:work --queue=default,import')->withoutOverlapping();
+        $schedule->command('queue:work --queue=default,import --max-jobs=30')->withoutOverlapping();
 
         $schedule->command('links:check')->everyTwoHours()->withoutOverlapping();
 
