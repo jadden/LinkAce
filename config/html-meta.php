42c7b918affb347089b7a18932ef0e3220d6e906
Kovah <mail@kovah.de>
2025-02-15 10:28:14 +0100

    Add missing default for meta generation timeout

--- config/html-meta.php
+++ config/html-meta.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'timeout' => env('META_GENERATION_TIMEOUT'),
+    'timeout' => env('META_GENERATION_TIMEOUT', 10),
     'parser' => \Kovah\HtmlMeta\HtmlMetaParser::class,
     'user_agents' => [
         env('APP_USER_AGENT', 'LinkAce/2 (https://github.com/Kovah/LinkAce)'),
